8500b744206b2f5757ed1e347d959c4ffb73846c
LuizHNR <luizhneri12@gmail.com>
2024-08-09 09:08:55 -0300

    atualização vetores

--- revisaoJava/src/listas/RevisaoVetor.java
+++ revisaoJava/src/listas/RevisaoVetor.java
@@ -2,6 +2,20 @@ package listas;
 
 public class RevisaoVetor {
 
-    //Conjunto de N elementos de mesmo tipo
-    String[] alunos = new String[5];
+    public static void main(String[] args) {
+
+        //Conjunto de N elementos de mesmo tipo
+        String[] alunos = new String[5];
+
+        alunos[0] = "Erick";
+        alunos[1] = "Luiz";
+        alunos[2] = "LuizH";
+        alunos[3] = "Vicenzo";
+        alunos[4] = "Pavam";
+
+        System.out.println("Lista de Alunos:\n");
+        for(int i = 0; i < 5; i++){
+            System.out.println( i+1 + alunos[i] );
+        }
+    }
 }
