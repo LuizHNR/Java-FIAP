d4d5afe88d8544ce7227de05a8dd855a308e70f8
LuizHNR <luizhneri12@gmail.com>
2024-08-22 09:36:09 -0300

    prepareStatement

--- agendaTelefonica/src/dao/ContatoDao.java
+++ agendaTelefonica/src/dao/ContatoDao.java
@@ -1,4 +1,32 @@
 package dao;
+import models.Contato;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+//É a classe responsavel por manipular os dados no BD
+//Implementando o CRUD( cadastros, consultas, alterações e exclusões)
 
 public class ContatoDao {
+    private Connection conexao;
+
+    public void cadastrarContato(Contato contato){
+        conexao = ConnectionFactory.obterConexao();
+        PreparedStatement comandoSQL = null;
+        try{
+            String sql = "insert into tbl_contato(ID_CONTATO,NOME_CONTATO,CELULAR_CONTATO,EMAIL_CONTATO,INSTAGRAM,TIPO) values (?,?,?,?,?,?)";
+            //O preparedStatement peo o comendo sql e executa no servidor oracle
+            //usando a conexão q foi configurada
+            comandoSQL = conexao.prepareStatement(sql);
+            comandoSQL.setInt(1,contato.getCodigo());
+            comandoSQL.setString(2,contato.getNome());
+            comandoSQL.setString(3,contato.getTelefone());
+            comandoSQL.setString(4,contato.getEmail());
+            comandoSQL.setString(5, contato.getInstagram());
+            comandoSQL.setString(6, contato.getTipoContato().toString());
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- agendaTelefonica/src/models/Contato.java
+++ agendaTelefonica/src/models/Contato.java
@@ -17,4 +17,51 @@ public class Contato {
     private String instagram;
     private TipoContatoEnum tipoContato;
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public TipoContatoEnum getTipoContato() {
+        return tipoContato;
+    }
+
+    public void setTipoContato(TipoContatoEnum tipoContato) {
+        this.tipoContato = tipoContato;
+    }
 }
